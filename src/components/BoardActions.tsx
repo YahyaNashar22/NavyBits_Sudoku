af0540e57fef0dcfefe3cc7d2246dd8644ecdde1
YahyaNashar22 <yahyanashar22@gmail.com>
2024-11-09 16:40:21 +0200

    refactored board actions to a seperate component

---
+++ src/components/BoardActions.tsx
@@ -0,0 +1,75 @@
+import { useGameLogicStore } from "../../store";
+
+const BoardActions = ({
+  setIsCompleted,
+}: {
+  setIsCompleted: (bool: boolean) => void;
+}) => {
+  const {
+    values,
+    errorExists,
+    setAlertVisible,
+    revealHint,
+    selectedDifficulty,
+    solvePuzzle,
+  } = useGameLogicStore();
+
+  const checkResult = () => {
+    if (errorExists || Object.keys(values).length === 0) {
+      setAlertVisible("Incomplete_puzzle", true);
+      return;
+    }
+    for (const cellId in values) {
+      const cellValue = values[cellId].value;
+      if (cellValue == null || cellValue == undefined) {
+        setAlertVisible("Incomplete_puzzle", true);
+        return;
+      }
+    }
+    setIsCompleted(true);
+  };
+
+  const customBoardHint = () => {
+    if (errorExists && selectedDifficulty === "custom") {
+      setAlertVisible("error_exists", true);
+    } else {
+      revealHint();
+    }
+  };
+
+  const customBoardSolver = () => {
+    if (errorExists && selectedDifficulty === "custom") {
+      setAlertVisible("error_exists", true);
+    } else {
+      solvePuzzle();
+    }
+  };
+
+  return (
+    <div className="btn_container">
+      <button
+        type="button"
+        className="btn check_result_btn"
+        onClick={checkResult}
+      >
+        Check Result
+      </button>
+
+      <button type="button" className="btn hint_btn" onClick={customBoardHint}>
+        💡 Hint
+      </button>
+
+      {selectedDifficulty === "custom" && (
+        <button
+          type="button"
+          className="btn hint_btn"
+          onClick={customBoardSolver}
+        >
+          Solve
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default BoardActions;
--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -3,50 +3,13 @@ import { useGameLogicStore } from "../../store";
 import Board from "../components/Board";
 import CustomAlert from "../components/CustomAlert";
 import Congratulations from "../components/Congratulations";
+import BoardActions from "../components/BoardActions";
 
 const Game = () => {
-  const {
-    values,
-    errorExists,
-    setAlertVisible,
-    alerts,
-    revealHint,
-    selectedDifficulty,
-    solvePuzzle,
-  } = useGameLogicStore();
+  const { values, setAlertVisible, alerts, selectedDifficulty } =
+    useGameLogicStore();
 
   const [isCompleted, setIsCompleted] = useState<boolean>(false);
-
-  const checkResult = () => {
-    if (errorExists || Object.keys(values).length === 0) {
-      setAlertVisible("Incomplete_puzzle", true);
-      return;
-    }
-    for (const cellId in values) {
-      const cellValue = values[cellId].value;
-      if (cellValue == null || cellValue == undefined) {
-        setAlertVisible("Incomplete_puzzle", true);
-        return;
-      }
-    }
-    setIsCompleted(true);
-  };
-
-  const customBoardHint = () => {
-    if (errorExists && selectedDifficulty === "custom") {
-      setAlertVisible("error_exists", true);
-    } else {
-      revealHint();
-    }
-  };
-
-  const customBoardSolver = () => {
-    if (errorExists && selectedDifficulty === "custom") {
-      setAlertVisible("error_exists", true);
-    } else {
-      solvePuzzle();
-    }
-  };
 
   console.log(values);
 
@@ -59,33 +22,7 @@ const Game = () => {
             : `Puzzle: ${selectedDifficulty}`}
         </h1>
         <Board />
-        <div className="btn_container">
-          <button
-            type="button"
-            className="btn check_result_btn"
-            onClick={checkResult}
-          >
-            Check Result
-          </button>
-
-          <button
-            type="button"
-            className="btn hint_btn"
-            onClick={customBoardHint}
-          >
-            💡 Hint
-          </button>
-
-          {selectedDifficulty === "custom" && (
-            <button
-              type="button"
-              className="btn hint_btn"
-              onClick={customBoardSolver}
-            >
-              Solve
-            </button>
-          )}
-        </div>
+        <BoardActions setIsCompleted={setIsCompleted} />
       </main>
       {alerts["Incomplete_puzzle"] && (
         <CustomAlert
