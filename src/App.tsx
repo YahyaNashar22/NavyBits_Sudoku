3b2a45931320c430c8a5b8dd80194cd53fd7f55a
YahyaNashar22 <yahyanashar22@gmail.com>
2024-11-13 18:00:16 +0200

    fixed back button on mobile phone navigates back through pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { useNavigate } from "react-router-dom";
 import "./App.css";
 import AppRoutes from "./routes/AppRoutes";
+import { useEffect } from "react";
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleBackButton = (event: PopStateEvent) => {
+      // Prevents default back action
+      event.preventDefault();
+      // Redirects to the home page
+      navigate("/");
+    };
+
+    // Attach listener to popstate event
+    window.addEventListener("popstate", handleBackButton);
+
+    // Cleanup on component unmount
+    return () => window.removeEventListener("popstate", handleBackButton);
+  }, [navigate]);
   return <AppRoutes />;
 }
 
