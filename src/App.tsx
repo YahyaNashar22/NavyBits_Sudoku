760b02a0a45faa8a2503aa0be0642cd28fb7a621
YahyaNashar22 <yahyanashar22@gmail.com>
2024-11-13 18:03:18 +0200

    roll back

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,7 @@
-import { useNavigate } from "react-router-dom";
 import "./App.css";
 import AppRoutes from "./routes/AppRoutes";
-import { useEffect } from "react";
 
 function App() {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const handleBackButton = (event: PopStateEvent) => {
-      // Prevents default back action
-      event.preventDefault();
-      // Redirects to the home page
-      navigate("/");
-    };
-
-    // Attach listener to popstate event
-    window.addEventListener("popstate", handleBackButton);
-
-    // Cleanup on component unmount
-    return () => window.removeEventListener("popstate", handleBackButton);
-  }, [navigate]);
   return <AppRoutes />;
 }
 
